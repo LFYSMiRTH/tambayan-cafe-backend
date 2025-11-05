f7380f400ae8fd5ca8d59c94af474bb6387c9130
LFYSMiRTH <johntimothyyanto64@gmail.com>
2025-11-05 20:53:00 +0800

    Fix readd

---
+++ Controllers/DevController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using TambayanCafeAPI.Models;
+
+namespace TambayanCafeAPI.Controllers
+{
+    [ApiController]
+    [Route("api/dev")]
+    public class DevController : ControllerBase
+    {
+        private readonly IMongoDatabase _database;
+
+        public DevController(IMongoDatabase database)
+        {
+            _database = database;
+        }
+
+        [HttpPost("hash-passwords")]
+        public async Task<IActionResult> HashAllPasswords()
+        {
+            var users = _database.GetCollection<User>("users");
+            var all = await users.Find(_ => true).ToListAsync();
+
+            foreach (var user in all)
+            {
+                // Skip if already hashed
+                if (user.Password.StartsWith("$2"))
+                    continue;
+
+                // Hash plaintext password
+                string hashed = BCrypt.Net.BCrypt.HashPassword(user.Password);
+                await users.UpdateOneAsync(
+                    u => u.Id == user.Id,
+                    Builders<User>.Update.Set(u => u.Password, hashed)
+                );
+            }
+
+            return Ok("All passwords hashed.");
+        }
+    }
+}
