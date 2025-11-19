bca89c776dea4fb9288e76fa18d52c45abd7c4ee
LFYSMiRTH <johntimothyyanto64@gmail.com>
2025-11-19 18:02:20 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -117,7 +117,9 @@ builder.Services.AddCors(options =>
                 "http://127.0.0.1:5500",
                 "http://localhost:5500",
                 "http://localhost:3000",
-                "http://127.0.0.1:3000"
+                "http://127.0.0.1:3000",
+                "http://127.0.0.1:5501", 
+                "http://localhost:5501"
             };
             return localOrigins.Contains(cleanOrigin);
         })
