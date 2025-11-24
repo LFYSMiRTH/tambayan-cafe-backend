9f1e0f5e0e4c21574e8c9d70a18fd58e499f5d36
LFYSMiRTH <johntimothyyanto64@gmail.com>
2025-11-24 20:31:27 +0800

    OrderRequestDto

--- Models/OrderRequestDto.cs
+++ Models/OrderRequestDto.cs
@@ -1,21 +1,29 @@
-﻿using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace TambayanCafeAPI.Models
 {
     public class OrderRequestDto
     {
-        public string CustomerId { get; set; }
-        public string CustomerEmail { get; set; }
-        public List<OrderItemDto> Items { get; set; }
+        [Required]
+        public string CustomerId { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string CustomerEmail { get; set; } = string.Empty;
+
+        [Required]
+        public List<OrderItemDto> Items { get; set; } = new();
+
+        [Required]
         public decimal TotalAmount { get; set; }
     }
 
     public class OrderItemDto
     {
-        public string ProductId { get; set; }
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-        public int Quantity { get; set; }
+        [Required] public string ProductId { get; set; } = string.Empty;
+        [Required] public string Name { get; set; } = string.Empty;
+        [Required] public decimal Price { get; set; }
+        [Required] public int Quantity { get; set; }
         public string Size { get; set; }
         public string Mood { get; set; }
         public string Sugar { get; set; }
