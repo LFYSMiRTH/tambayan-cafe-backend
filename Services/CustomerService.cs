20c307e779ad12ea89c7d0abc51759871876a9ba
LFYSMiRTH <johntimothyyanto64@gmail.com>
2025-11-25 06:54:19 +0800

    CustomerService

--- Services/CustomerService.cs
+++ Services/CustomerService.cs
@@ -7,11 +7,11 @@ namespace TambayanCafeAPI.Services
 {
     public class CustomerService : ICustomerService
     {
-        private readonly IMongoCollection<Customer> _customers;
+        private readonly IMongoCollection<User> _users;
 
         public CustomerService(IMongoDatabase database)
         {
-            _customers = database.GetCollection<Customer>("users");
+            _users = database.GetCollection<User>("users");
         }
 
         public async Task<Customer> GetByIdAsync(string id)
@@ -19,8 +19,28 @@ namespace TambayanCafeAPI.Services
             if (string.IsNullOrWhiteSpace(id) || !ObjectId.TryParse(id, out var objectId))
                 return null;
 
-            var filter = Builders<Customer>.Filter.Eq("_id", objectId);
-            return await _customers.Find(filter).FirstOrDefaultAsync();
+            var filter = Builders<User>.Filter.Eq(u => u.Id, id);
+            var projection = Builders<User>.Projection
+                .Include(u => u.Id)
+                .Include(u => u.Name)
+                .Include(u => u.FirstName)
+                .Include(u => u.LastName)
+                .Include(u => u.Username)
+                .Include(u => u.Email);
+
+            var user = await _users.Find(filter).Project<User>(projection).FirstOrDefaultAsync();
+            if (user == null)
+                return null;
+
+            return new Customer
+            {
+                Id = user.Id,
+                Name = user.Name,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Username = user.Username,
+                Email = user.Email
+            };
         }
     }
 }
